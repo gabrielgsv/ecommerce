562de985c197741c7a14e907dad5717671d78766
gabrielgsv <gabriel.soares.vilela@gmail.com>
2020-07-19 15:51:30 -0300

    add reducers for clothing and shoes

---
+++ src/store/reducers/Clothing.js
@@ -0,0 +1,10 @@
+import clothing from "../../utils/clothing"
+
+const Clothing = (state = clothing, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default Clothing
---
+++ src/utils/shoes.js
@@ -0,0 +1,15 @@
+import botaFemininaImage from "../images/shoes/bota-feminina.jpg"
+import newBalanceImage from "../images/shoes/new-balance.jpg"
+import nikeImage from "../images/shoes/nike.jpg"
+import sapatoFemininoAzulImage from "../images/shoes/sapato-feminino-azul.jpg"
+import tenisVermelhoImage from "../images/shoes/tenis-vermelho.jpg"
+
+const clothing = [
+  {id: 6, title: "Bota Feminina", image: botaFemininaImage, price: 119.90},
+  {id: 7, title: "New Balance", image: newBalanceImage, price: 109.90},
+  {id: 8, title: "Nike", image: nikeImage, price: 139.90},
+  {id: 9, title: "Sapato Feminino Azul", image: sapatoFemininoAzulImage, price: 129.90},
+  {id: 10, title: "Tênis Vermelho", image: tenisVermelhoImage, price: 99.90}
+]
+
+export default clothing
